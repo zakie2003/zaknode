46b7b4f36727b334abd5960ea1799ef20fba9bb2
zakie2003 <121384625+zakie2003@users.noreply.github.com>
2023-07-20 09:36:44 +0530

    tke it

--- functions/homepage.js
+++ functions/homepage.js
@@ -4,20 +4,24 @@ const path = require("path");
 
 const app=express();
 
+const serverless=require("serverless-http");
+
+const router=express.Router();
+
 app.set("view engine","ejs");
 
 const pubDirectory = path.join(__dirname,"./public");
 
 app.use(express.static(pubDirectory));
 
-app.get("/",(req,res,next)=>{
+router.get("/",(req,res,next)=>{
     res.render("index.ejs");
 })
 
-app.post("/proj",(req,res,next)=>{
+router.post("/proj",(req,res,next)=>{
     res.render("project.ejs");
 })
 
-app.listen(3000,()=>{
-    console.log("Server started");
-})
+app.use("/.functions/homepage",router);
+
+module.exports.handler=serverless(app)
